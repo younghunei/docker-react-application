4711582a075cc8afa9e91a08342f4c907ee66862
younghunei <cyh5833@naver.com>
2022-11-29 12:30:24 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          주어나 사랑해헤헤헿vnvn
         </a>
       </header>
     </div>
